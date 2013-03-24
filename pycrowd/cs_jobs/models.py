6b84b94dd5b8bee34bdbb131164665158539e7d1
Bryan Marty <bxm156@case.edu>
2013-03-24 13:04:45 -0700

    jobs should use new Django 1.5 user profile

--- pycrowd/cs_jobs/models.py
+++ pycrowd/cs_jobs/models.py
@@ -4,11 +4,11 @@ Created on Nov 6, 2012
 @author: Bryan
 '''
 from django.db import models
-import pycrowd.settings as settings
+from django.conf import settings
 
 class CrowdsourceJob(models.Model):
     name = models.CharField(max_length=30)
-    owner = models.ForeignKey(settings.PROFILE_MODULE,editable=False)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,editable=False)
     instructions = models.TextField()
     target_accuracy = models.IntegerField()
     task_count = models.IntegerField(editable=False)
